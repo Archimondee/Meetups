27acd66de2e735a4ac99bc66bf5a298aa5b3d2b1
aditya1234 <kikchiemedia@gmail.com>
2018-02-27 00:33:34 +0700

    First Init

---
+++ meetup-backend/server/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import dbConfig from './config/db';
+import middlewareConfig from './config/middlewares';
+import {MeetupRoutes} from './modules';
+
+const app = express();
+
+/**
+ * Database
+ */
+dbConfig();
+
+/**
+ * Middlewares
+ */
+middlewareConfig(app);
+
+app.use('/api', [MeetupRoutes]);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, err => {
+    if(err){
+        console.error(err);
+    }{
+        console.log('App listen in 3000');
+    }
+})
